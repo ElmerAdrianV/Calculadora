d31d976ac4797e3b9e5d2cde91a0ea7556766faf
ElmerAdrianV <elmeradrianv@outlook.com>
2021-02-13 15:18:04 -0600

    Hice la funcion de verificar

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -5,15 +5,179 @@
  */
 package calculadora;
 
+import pila.PilaA;
+
 /**
  *
- * @author elmer
+ * @author ElmerAdrianV
+ * @author bobadilla E
  */
 public class Calculadora {
 
-        private String Cadena;
+    private String operaciones;
+    private String resultado;
+
+    public Calculadora(){
+    }    
+    
+    /**
+     *@return <pre>regresa el resultado de las operaciones 
+     * guardadas como atributo actualmente
+     * </pre>
+     */
+    public String getResultado() {
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculadora: \n" + "operaciones=" + operaciones + ", resultado=" + resultado + '\n';
+    }
+    
+    public boolean isNumber(char a){
+        boolean resp= true;
+        switch(a){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                break;
+            default:
+                resp= false;
+                break;
+        }
+        return resp;
+    }
+    
+    public boolean isOperator(char a){
+        boolean resp= true;
+        switch(a){
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+            case '^':
+                break;
+            default:
+                resp= false;
+                break;
+        }
+        return resp;
+    }
+    
+    private boolean verificarOperaciones(String cadena){
+        boolean resp=true;
+        PilaA<Character> parentesis = new PilaA<>();
+        int i=0, n=cadena.length();
+        char a;
         
-        public Calculadora(){
+        while(i<n && resp){
+            switch(cadena.charAt(i)){
+                case '(':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        
+                        if( a==')' || a=='*' || a=='/' || a=='^')
+                            resp=false;
+                        
+                        else
+                            parentesis.push('(');
+                    }
+                    else
+                        resp=false;
+                    break;
+                case ')':
+                    if(!parentesis.isEmpty())
+                        parentesis.pop();
+                    else
+                        resp=false;
+                    break; 
+                case'+':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        if( a=='^' || a=='*' || a=='/' || a==')' ){
+                            resp=false;
+                        }
+                    }
+                    else
+                        resp=false;
+
+                    break;
+                case'-':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        if( a=='^' || a=='*' || a=='/' || a==')' ){
+                            resp=false;
+                        }
+                    }
+                    else
+                        resp=false;
+                    break;
+                case'*':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        if( a=='^' || a=='*' || a=='/' || a==')'){
+                            resp=false;
+                        }
+                    }
+                    else
+                        resp=false;
+                    break;
+                case'/':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        if( a=='^' || a=='*' || a=='/' || a==')'){
+                            resp=false;
+                        }
+                    }
+                    else
+                        resp=false;
+                    break;
+                case'^':
+                    if(i+1<n){
+                        a=cadena.charAt(i+1);
+                        if( a=='^' || a=='*' || a=='/' || a==')' ){
+                            resp=false;
+                        }
+                    }
+                    else
+                        resp=false;
+                    break;
+            }
+            i++;
+        }
+        return resp;
+    }
+    
+    /*
+    private boolean verificarOperaciones(String cadena){
+        boolean resp=true;
+        PilaA <String> aux= new PilaA<>();
+        int i=0,j, n= cadena.length();
+        StringBuilder sb= new StringBuilder();
+        
+        while(i<n && resp){
+            j=i;
+            cadena.charAt(i);
+            while(j<n && resp){
+                
+            }
             
-        }    
+        }
+        
+        
+        return resp;
+    }
+      */     
+    public String algortimoPosfija(String cadena){
+
+        return "Hola";
+    }
+
 }
